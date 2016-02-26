7fafcaef5a43318e84c02462abd39ede8928a535
Juzer Shabbir Husain <juzer.s007@gmail.com>
2016-02-26 13:50:05 +0400

    Added solution to problem 1068. Sum

---
+++ src/com/littleibex/timus/problems/Sum_1068.java
@@ -0,0 +1,24 @@
+package com.littleibex.timus.problems;
+
+import com.littleibex.timus.utils.InputReader;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+/**
+ * @see <a href="http://acm.timus.ru/problem.aspx?space=1&num=1068">1068. 1, 10, 100, 1000...</a>
+ */
+public class Sum_1068 {
+
+    public static void main(String[] args) throws Exception {
+        InputReader reader = new InputReader(System.in);
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int N = reader.readInt();
+        writer.print(N > 0 ? N * (N + 1) / 2 : N * (N - 1) / -2 + 1);
+
+        writer.flush();
+        writer.close();
+    }
+}
