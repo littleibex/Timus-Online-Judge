b14a805f3b7f2d17b1eb90507972dddca29e7aeb
Juzer Shabbir Husain <juzer.s007@gmail.com>
2016-02-29 21:21:54 +0400

    Added solution to problem 1225. Flags

---
+++ src/com/littleibex/timus/problems/Flags_1225.java
@@ -0,0 +1,27 @@
+package com.littleibex.timus.problems;
+
+import com.littleibex.timus.utils.IOUtils;
+
+import java.io.IOException;
+
+/**
+ * @see <a href="http://acm.timus.ru/problem.aspx?space=1&num=1225">1225. Flags</a>
+ */
+public class Flags_1225 {
+
+    public static void main(String[] args) throws IOException {
+        new Flags_1225().run();
+    }
+
+    public void run() throws IOException {
+        int n = IOUtils.readPositiveInt() - 2;
+        long res = 2, n1 = 2, n2 = 2;
+        while (n > 0) {
+            res = n1 + n2;
+            n1 = n2;
+            n2 = res;
+            n--;
+        }
+        System.out.print(res);
+    }
+}
