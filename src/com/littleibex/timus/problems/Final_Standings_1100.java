8602499e036d8a9774473c0826fc56ee7cc795bd
Juzer Shabbir Husain <juzer.s007@gmail.com>
2016-03-04 22:23:41 +0400

    Solution to problem 1100. Final Standings - Bucket sort implementation

--- src/com/littleibex/timus/problems/Final_Standings_1100.java
+++ src/com/littleibex/timus/problems/Final_Standings_1100.java
@@ -3,16 +3,13 @@ package com.littleibex.timus.problems;
 import com.littleibex.timus.utils.IOUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @see <a href="http://acm.timus.ru/problem.aspx?space=1&num=1100">1100. Final Standings</a>
  */
 public class Final_Standings_1100 {
-
-    private Team[] teams;
-    private Team[] tmp;
-
-    private int i, j;
 
     public static void main(String[] args) throws IOException {
         new Final_Standings_1100().run();
@@ -22,56 +19,27 @@ public class Final_Standings_1100 {
         IOUtils io = new IOUtils();
 
         int n = io.readPositiveInt();
-        teams = new Team[n];
+        String id;
 
-        for (i = 0; i < n; i++) {
-            teams[i] = new Team();
-            teams[i].id = io.readString();
-            teams[i].score = io.readPositiveInt();
+        List<String>[] teams = new ArrayList[101];
+        for (int i = 0; i <= 100; i++) {
+            teams[i] = new ArrayList<>();
         }
 
-        tmp = new Team[n];
+        while (n > 0) {
+            id = io.readString();
+            teams[io.readPositiveInt()].add(id);
+            n--;
+        }
 
-        mergesort(0, n - 1);
-
-        for (i = 0; i < n; i++) {
-            io.writer.print(teams[i].id);
-            io.writer.print(' ');
-            io.writer.println(teams[i].score);
+        for (int i = 100; i >= 0; i--) {
+            for (String team : teams[i]) {
+                io.writer.print(team);
+                io.writer.print(' ');
+                io.writer.println(i);
+            }
         }
 
         io.shutdown();
-    }
-
-    private void mergesort(int p, int r) {
-        if (p < r) {
-            int q = (p + r) / 2;
-            mergesort(p, q);
-            mergesort(q + 1, r);
-            merge(p, q, r);
-        }
-    }
-
-    private void merge(int p, int q, int r) {
-        for (i = p; i <= r; i++) {
-            tmp[i] = teams[i];
-        }
-        i = p;
-        j = q + 1;
-        while (i <= q && j <= r) {
-            if (tmp[i].score >= tmp[j].score) {
-                teams[p++] = tmp[i++];
-            } else {
-                teams[p++] = tmp[j++];
-            }
-        }
-        while (i <= q) {
-            teams[p++] = tmp[i++];
-        }
-    }
-
-    private static class Team {
-        public String id;
-        public int score;
     }
 }
